31bed886415d2f7285e29428a5c16d694b7f0785
Daniel <dani2014games@gmail.com>
2022-08-06 01:09:37 -0300

    Versão 06/08

--- AplicacaoWeb/db/db.go
+++ AplicacaoWeb/db/db.go
@@ -9,10 +9,11 @@ import (
 
 //CRIANDO FUNÇÃO DE CONECTAR COM BANCO DE DADOS
 func ConectaComBancoDeDados() *sql.DB {
-	conexao := "user=root dbname=root password = root host = localhost port = 5432 sslmode = disable" //Passa parâmetros de coneo
-	db, err := sql.Open("postgres", conexao)                                                       //Abre db
-	if err != nil {                                                                                //Verifica se há erro
+	conexao := "host=localhost user=root password=root dbname=root port=5432  sslmode=disable" //Passa parâmetros de coneo
+	db, err := sql.Open("postgres", conexao)                                                   //Abre db
+	if err != nil {                                                                            //Verifica se há erro
 		panic(err.Error())
 	}
 	return db
+
 }
