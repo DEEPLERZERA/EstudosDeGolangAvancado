9fe09455cca38c51495eb9c779a27fccd49dca27
Daniel <dani2014games@gmail.com>
2022-08-18 13:23:52 -0300

    Versão 18/08

--- ProjetoChi/httpd/handler/newsfeed_get_test.go
+++ ProjetoChi/httpd/handler/newsfeed_get_test.go
@@ -1,3 +1,32 @@
 package handler
 
-//A fazer :D
+import (
+	"ProjetosDeGolang/ProjetoChi/platform/newsfeed"
+	"ProjetosDeGolang/ProjetoChi/platform/newsfeed/mock_http"
+	"net/http"
+	"testing"
+)
+
+func TestNewsFeedGet(t *testing.T) {
+	feed := newsfeed.New()
+	feed.Add(newsfeed.Item{"hello", "World"}) // add an item to the feed
+
+	handler := NewsFeedGet(feed)
+
+	w := &mock_http.ResponseWriter{}
+
+	r := &http.Request{}
+
+	handler(w, r)
+
+	result := w.GetBodyJSONArray()
+
+	if len(result) !=  1 {
+		t.Errorf("Item was not added to the datastore")
+	}
+
+	if result[0]["title"] != "hello" {
+		t.Errorf("Item was not properly set")
+	}
+
+}
--- ProjetoChi/httpd/handler/newsfeed_post_test.go
+++ ProjetoChi/httpd/handler/newsfeed_post_test.go
@@ -1,3 +1,44 @@
 package handler
 
-//A fazer :D
+import (
+	"ProjetosDeGolang/ProjetoChi/platform/newsfeed"
+	"ProjetosDeGolang/ProjetoChi/platform/newsfeed/mock_http"
+	"net/http"
+	"testing"
+)
+
+func TestNewsfeedPost(t *testing.T) {
+	feed := newsfeed.New()
+
+	headers := http.Header{}
+	headers.Add("Content-Type", "application/json")
+
+	w := &mock_http.ResponseWriter{}
+	r := &http.Request{
+		Header: headers,
+	}
+
+	r.Body = mock_http.RequestBody(map[string]string{
+		"title" : "Legal",
+		"post"  : "anime",
+	})
+
+	handler := NewsFeedPost(feed)
+
+	handler(w, r)
+
+	result:= w.GetBodyString()
+
+	if result != "Good job!" {
+		t.Errorf("Handler did not complete")
+	}
+
+	if len(feed.GetAll()) != 1 {
+		t.Errorf("Item did not add")
+	}
+
+	if feed.GetAll()[0].Title != "hello" {
+		t.Errorf("Item bad")
+	}
+
+}
